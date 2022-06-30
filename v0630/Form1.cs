6b287a4f6c1f4e0d03d973c98f7950f3c0c91a25
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-06-30 16:08:55 +0900

    配列

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,13 +12,8 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int vx1 = rand.Next(-10, 11);
-        int vy1 = rand.Next(-10, 11);
-        int vx2 = rand.Next(-10, 11);
-        int vy2 = rand.Next(-10, 11);
-        int vx3 = rand.Next(-10, 11);
-        int vy3 = rand.Next(-10, 11);
-        int point = 100;
+        int[] vx = new int[3];
+        int[] vy = new int[3];
         //静的=最初に決めておく <> 動的=実行時変更可能
         static Random rand = new Random();
                    //変数名
@@ -26,7 +21,13 @@ namespace v0630
         {
             InitializeComponent();
 
-           
+            vx[0] = rand.Next(-10,11);
+            vy[0] = rand.Next(-10,11);
+            vx[1] = rand.Next(-10, 11);
+            vy[1] = rand.Next(-10,11);
+            vx[2] = rand.Next(-10,11);
+            vy[2] = rand.Next(-10, 11);
+
             label1.Left = rand.Next(ClientSize.Width-label1.Width);
             label1.Top = rand.Next(ClientSize.Height-label1.Height); 
             label3.Left = rand.Next(ClientSize.Width - label3.Width);
@@ -55,94 +56,95 @@ namespace v0630
             label2.Left = fpos.X - label2.Width / 2; //-ラベルの幅の半分
             label2.Top = fpos.Y - label2.Height / 2;
 
-            label1.Left = label1.Left + vx1;
-            label1.Top = label1.Top + vy1;
-            label3.Top = label3.Top+ vy2 ;
-            label3.Left = label3.Left+vx2;
-            label4.Top = label4.Top + vy3;
-            label4.Left = label4.Left + vx3;
+            label1.Left = label1.Left + vx[0];
+            label1.Top = label1.Top + vy[0];
+            label3.Top = label3.Top+ vy[1] ;
+            label3.Left = label3.Left+vx[1];
+            label4.Top = label4.Top + vy[2];
+            label4.Left = label4.Left + vx[2];
 
             if (label1.Left < 0)
                
             {
-                vx1 = Math.Abs(vx1); 
+                vx[0] = Math.Abs(vx[0]); 
                
             }
 
             if (label1.Right > ClientSize.Width)
             
             {
-                vx1 = -Math.Abs(vx1);
+                vx[0] = -Math.Abs(vx[0]);
               
             }
             if (label1.Bottom > ClientSize.Height)
             {
-                vy1 = -Math.Abs(vy1);
+                vy[0] = -Math.Abs(vy[0]);
               
             }
             if (label1.Top < 0)
             {
-                vy1 = Math.Abs(vy1);
+                vy[0] = Math.Abs(vy[0]);
                
             }
             //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
             if (label3.Left < 0)
 
             {
-                vx2 = Math.Abs(vx2);
+                vx[1] = Math.Abs(vx[1]);
 
             }
 
             if (label3.Right > ClientSize.Width)
 
             {
-                vx2 = -Math.Abs(vx2);
+                vx[1] = -Math.Abs(vx[1]);
 
             }
             if (label3.Bottom > ClientSize.Height)
             {
-                vy2 = -Math.Abs(vy2);
+                vy[1] = -Math.Abs(vy[1]);
 
             }
             if (label3.Top < 0)
             {
-                vy2 = Math.Abs(vy2);
+                vy[1] = Math.Abs(vy[1]);
 
             }
             //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
             if (label4.Left < 0)
 
             {
-                vx3 = Math.Abs(vx3);
+                vx[2] = Math.Abs(vx[2]);
 
             }
 
             if (label4.Right > ClientSize.Width)
 
             {
-                vx3 = -Math.Abs(vx3);
+                vx[2] = -Math.Abs(vx[2]);
 
             }
             if (label4.Bottom > ClientSize.Height)
             {
-                vy3 = -Math.Abs(vy3);
+                vy[2] = -Math.Abs(vy[2]);
 
             }
             if (label4.Top < 0)
             {
-                vy3 = Math.Abs(vy3);
+                vy[2] = Math.Abs(vy[2]);
 
             }
             if ((fpos.X >=label1.Left)
               && (fpos.X<label1.Right )
               && (fpos.Y>=label1.Top)
               && (fpos.Y<label1.Bottom)
-             ||
-              (fpos.X >= label3.Left)
+              ||
+                 (fpos.X >= label3.Left)
               && (fpos.X < label3.Right)
               && (fpos.Y >= label3.Top)
               && (fpos.Y < label3.Bottom)
-              ||(fpos.X >= label4.Left)
+              ||
+                 (fpos.X >= label4.Left)
               && (fpos.X < label4.Right)
               && (fpos.Y >= label4.Top)
               && (fpos.Y < label4.Bottom)
