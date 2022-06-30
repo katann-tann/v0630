805fdb8631636d216eb638cdc0fd0d0b518e4588
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-06-30 13:38:45 +0900

    経過報告

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -16,6 +16,45 @@ namespace v0630
         {
             InitializeComponent();
         }
+        int vx = -5;
+        int vy = -10;
+      
+        private void label1_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label2.Left = fpos.X - label2.Width / 2; //-ラベルの幅の半分
+            label2.Top = fpos.Y - label2.Height / 2;
+
+            label1.Left = label1.Left + vx;
+            label1.Top = label1.Top + vy;
+           if(label1.Bottom < 0)
+            {
+                vx=+ 5;
+            }
+           if(label1.Bottom > ClientSize.Height)
+            {
+                vy = +-5;
+            }
+            if(label1.Bottom > ClientSize.Width)
+            {
+               vy = +10;
+            }
+            if (label1.Bottom < ClientSize.Height)
+            {
+                vx = +-10;
+            }
+            
+        }
     }
 }
